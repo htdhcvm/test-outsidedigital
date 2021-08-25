dffa545b0ccc9ea4a528dad21a8ef97d4df99890
htdhcvm <62516119+htdhcvm@users.noreply.github.com>
2021-08-25 22:47:33 +0300

    set cors for all, body parser json, and connect to db

--- auth/config.ts
+++ auth/config.ts
@@ -1,9 +1,23 @@
 import { Express } from 'express';
-
 import * as dotenv from 'dotenv';
+import DbWorker from './models/DbWorker';
+import cors from 'cors';
+import route from './routes';
+import bodyParser from 'body-parser';
 
 const setConfigs = (app: Express) => {
     dotenv.config({ path: __dirname + '/.env' });
+    app.use(bodyParser.json());
+    app.use(
+        cors({
+            origin: '*',
+            methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+            credentials: true,
+        })
+    );
+    DbWorker.connect();
+
+    app.use('/', route);
 };
 
 export default setConfigs;
