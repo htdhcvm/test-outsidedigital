17cfea68d91890dd0b55d8935875548ca81c45d7
htdhcvm <62516119+htdhcvm@users.noreply.github.com>
2021-08-27 11:57:47 +0300

    add query for insert test data

--- api/migrations/1629901910150_tag.js
+++ api/migrations/1629901910150_tag.js
@@ -4,11 +4,22 @@ exports.shorthands = undefined;
 
 exports.up = (pgm) => {
     pgm.createTable('tag', {
-        id: { type: 'integer', notNull: true, primaryKey: true },
-        creator: { type: 'uuid', notNull: true },
+        id: { type: 'serial', notNull: true, primaryKey: true },
+        creator: { type: 'uuid' },
         name: { type: 'varchar(40)', notNull: true },
-        sortOrder: { type: 'integer', notNull: true, default: 0 },
+        sort_order: { type: 'integer', notNull: true, default: 0 },
     });
+
+    pgm.sql(`
+        INSERT INTO tag(id, name, sort_order)
+        VALUES
+            (1, 'Web', 0),
+            (2, 'Nodejs', 1),
+            (3, 'React', 2),
+            (4, 'Redis', 3),
+            (5, 'Redux', 4),
+            (6, 'Express', 6),
+            (7, 'Docker', 1)`);
 };
 
 exports.down = (pgm) => {
