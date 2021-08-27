cea0916d64d76fedd7a434ebdd6d30fc5d34d87b
htdhcvm <62516119+htdhcvm@users.noreply.github.com>
2021-08-27 11:59:32 +0300

    include body parser connect to db and routes

--- api/config.ts
+++ api/config.ts
@@ -1,9 +1,16 @@
 import { Express } from 'express';
-
 import * as dotenv from 'dotenv';
+import DbWorker from './models/DbWorker';
+import tagRoutes from './routes/TagRoutes';
+import userRoutes from './routes/UserRoutes';
+import bodyParser from 'body-parser';
 
 const setConfigs = (app: Express) => {
     dotenv.config({ path: __dirname + '/.env' });
+    app.use(bodyParser.json());
+    app.use('/', tagRoutes);
+    app.use('/', userRoutes);
+    DbWorker.connect();
 };
 
 export default setConfigs;
